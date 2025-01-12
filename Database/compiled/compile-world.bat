b78368ebf1817f550dac120fc1cbcb4195744cae
Aaron Baerbock <152320820+LingradAC@users.noreply.github.com>
2025-01-12 16:46:30 -0600

    Update compile-world.bat

--- Database/compiled/compile-world.bat
+++ Database/compiled/compile-world.bat
@@ -1,25 +1,25 @@
 @echo off
 IF EXIST *.sql del *.sql
 echo.
-echo Creating ACE-World-Database...
+echo Creating Chickentide-World-Database...
 echo.
-echo /* ACE-World-Database */ >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
-echo /* ACE-World-16PY */ >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
+echo /* Chickentide-World-Database */ >> Chickentide-World-Database.sql
+echo. >> Chickentide-World-Database.sql
+echo /* Chickentide-World */ >> Chickentide-World-Database.sql
+echo. >> Chickentide-World-Database.sql
 
 IF EXIST *.zip (
-    echo Found downloaded database zip file, skipping ACE-World-16PY Base compile...
+    echo Found downloaded database zip file, skipping Chickentide-World-16PY Base compile...
     echo.
-    7z x ACE-World-16PY-*.zip
-    copy /b ACE-World-Database.sql + ACE-World-16PY-*.sql ACE-World-Database.sql
-    echo. >> ACE-World-Database.sql
+    7z x Chickentide-World-*.zip
+    copy /b Chickentide-World-Database.sql + Chickentide-World-*.sql Chickentide-World-Database.sql
+    echo. >> Chickentide-World-Database.sql
 ) ELSE (
-    echo Compiling ACE-World-16PY Base...
+    echo Compiling Chickentide-World Base...
     call compile-base.bat
     @echo off
-    copy /b ACE-World-Database.sql + ..\ACE-World-16PY\Database\compiled\*.sql ACE-World-Database.sql
-    echo. >> ACE-World-Database.sql
+    copy /b Chickentide-World-Database.sql + ..\Chickentide-World\Database\compiled\*.sql Chickentide-World-Database.sql
+    echo. >> Chickentide-World-Database.sql
     set localdbcompile=1
 )
 
@@ -28,18 +28,18 @@ echo.
 echo Compiling Patches, skipping patches marked as optional...
 call compile-patches.bat
 @echo off
-echo /* ACE-World-16PY-Patches */ >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
+echo /* Chickentide-World-Patches */ >> Chickentide-World-Database.sql
+echo. >> Chickentide-World-Database.sql
 
 echo.
-copy /b ACE-World-Database.sql + ..\Patches\*.sql ACE-World-Database.sql
+copy /b Chickentide-World-Database.sql + ..\Patches\*.sql Chickentide-World-Database.sql
 echo.
 
-IF DEFINED "%localdbcompile%" move ..\ACE-World-16PY\Database\compiled\*.sql
+IF DEFINED "%localdbcompile%" move ..\Chickentide-World\Database\compiled\*.sql
 move ..\Patches\*.sql
 
-echo UPDATE `version` SET `patch_Version` = 'v%APPVEYOR_BUILD_VERSION%' WHERE (`id` = '1'); >> ACE-World-Database.sql
-echo. >> ACE-World-Database.sql
+echo UPDATE `version` SET `patch_Version` = 'v%APPVEYOR_BUILD_VERSION%' WHERE (`id` = '1'); >> Chickentide-World-Database.sql
+echo. >> Chickentide-World-Database.sql
 
 echo.
 @echo on
